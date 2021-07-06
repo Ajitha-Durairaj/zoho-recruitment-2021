c1b6c916508843e59a3fa3bed5dacd7205a75856
Ajitha-Durairaj <86967400+Ajitha-Durairaj@users.noreply.github.com>
2021-07-06 10:15:07 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test
+{
+	public static void main(String[] args) {
+		String input = "WELCOME";
+		String newInput = input.substring(input.length()/2) + input.substring(0,input.length()/2);
+		String printText = "";
+		for(int i=0;i<newInput.length();i++) {
+		    printText += newInput.charAt(i);
+		    for(int j=0;j<newInput.length()-i;j++)
+		        System.out.print(" ");
+		    System.out.print(printText);
+		    System.out.println("");
+		}
+	}
+}
